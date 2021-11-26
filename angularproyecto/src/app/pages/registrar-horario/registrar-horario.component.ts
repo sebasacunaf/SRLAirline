80bd0efb011343c3cb9b1b670f69b7b0c10e1bfa
SEBASTIAN ACUÑA FERNANDEZ <sacuna@est.utn.ac.cr>
2021-11-26 11:20:50 -0600

    correción edits

--- angularproyecto/src/app/pages/registrar-horario/registrar-horario.component.ts
+++ angularproyecto/src/app/pages/registrar-horario/registrar-horario.component.ts
@@ -12,6 +12,7 @@ export class RegistrarHorarioComponent implements OnInit {
   form: any = {
     ID_Ruta: null,
     Dia: null,
+    Duracion:null,
     HoraSalida: null,
     HoraLlegada: null,
     Precio: null,
